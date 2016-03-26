9af96d1d5639148cb1469177ba475bbf0d418a7b
Jozx <jsaracho1@gmail.com>
2016-03-26 15:59:08 -0300

    Creacion Modelos Cliente/Vehiculo

---
+++ GeoAgenda/GeoAgenda/Models/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeoAgenda.Models
+{
+    [Table("Clientes")]
+
+    public class Cliente
+    {
+        [Key]
+        public int IdCliente { get; set; }
+        public string RazonSocial { get; set; }
+        public string Direccion { get; set; }
+        public string NombreContacto { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public int RUC  { get; set; }
+        public int DV { get; set; }
+        public double latitud { get; set; }
+        public double longitud { get; set; }
+        public Boolean Estado { get; set; }
+    }
+}
---
+++ GeoAgenda/GeoAgenda/Models/Vehiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeoAgenda.Models
+{
+    [Table("Vehiculos")]
+    public class Vehiculo
+    {
+        [Key]
+        public int IdVehiculo { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string FechaFabricacion { get; set; }
+        public string Kilometraje { get; set; }
+        public string Conductor { get; set; }
+        public double latitudBase { get; set; }
+        public double longitudBase { get; set; }
+    }
+}
