18ef1fdfa805e95128b2743ba1078a4b6b24e54e
Jozx <jsaracho1@gmail.com>
2016-03-26 16:19:36 -0300

    - Creacion conexion DB

---
+++ GeoAgenda/GeoAgenda/Context/GeoAgendaContext.cs
@@ -0,0 +1,21 @@
+﻿using GeoAgenda.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GeoAgenda.Context
+{
+    public class GeoAgendalContext : DbContext
+    {
+        public GeoAgendalContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet <Cliente> Clientes { get; set; }
+        public DbSet <Vehiculo> Vehiculos { get; set; }
+
+    }
+}
